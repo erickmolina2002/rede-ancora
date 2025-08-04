7b0bb98ef3a1babf861b734855189d1719f3ce17
Erick Molina <erickmolina2002@hotmail.com>
2025-08-04 15:39:41 -0300

    feat: camera

--- src/app/ui/inputs/CameraInput.tsx
+++ src/app/ui/inputs/CameraInput.tsx
@@ -367,7 +367,7 @@ export default function CameraInput({
         </div>
       )}
 
-      {/* Camera Modal - Layout Simples */}
+      {/* Camera Modal - Layout  Simples */}
       {isCapturing && (
         <div className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50">
           <div className="bg-white p-4 rounded-lg max-w-sm w-full mx-4">
